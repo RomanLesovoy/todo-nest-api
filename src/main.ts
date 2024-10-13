3a08db1ca2d81f7ce63efdf70ba5cfd904c496eb
RomanLesovoy <romadesire97@gmail.com>
2024-10-13 19:59:24 +0300

    fix: allow list of origins

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
 
+  const whitelist = ['https://tickets-board-v1.firebaseapp.com', 'http://tickets-board-v1.firebaseapp.com/'];
   app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("Allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("Blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
     allowedHeaders:"*",
-    origin: true
   });
 
   app.useGlobalPipes(new ValidationPipe(
