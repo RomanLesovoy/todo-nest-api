bc2b6af5fab1fa4413f887c060594ff756969a36
RomanLesovoy <romadesire97@gmail.com>
2024-10-20 14:52:08 +0300

    fix: .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 import * as ormConfig from '../ormconfig.json';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
     ...ormConfig,
-  } as any), TodoModule],
+  } as any),
+  TodoModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
